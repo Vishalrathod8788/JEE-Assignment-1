bffdcfda046c5df21b4b154fb7d50532dcfaa481
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-08-03 23:31:03 +0530

    Add File

--- Assignment-1/src/assi/Pro_1.java
+++ Assignment-1/src/assi/Pro_1.java
@@ -1,32 +1,44 @@
 package assi;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Pro_1
- */
 @WebServlet("/Pro_1")
 public class Pro_1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-    /**
-     * Default constructor. 
-     */
-    public Pro_1() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
+	String URL, Username, Password, Qry;
+	Connection cnn;
+	
+	public void mySql()
+	{
+		URL = "jdbc:mysql://localhost:3306/mysql";
+		Username = "root";
+		Password = "";
+		
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			cnn = DriverManager.getConnection(URL);
+		} catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		
+	}
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+				
+		
 	}
 
 }
