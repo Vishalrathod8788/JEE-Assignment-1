606ca1ba5976a235db21e5405b93bf02104d2339
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-08-04 22:30:33 +0530

    Add Servlet - Pro_4

---
+++ Assignment-1/src/assi/Pro_4.java
@@ -0,0 +1,19 @@
+package assi;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Pro_4")
+public class Pro_4 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		
+	}
+
+}
