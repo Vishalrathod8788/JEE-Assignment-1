3c7319c5c1f6503c426c61431705383379c3758f
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-08-08 22:28:10 +0530

    Add_pro_8

---
+++ Pro_8/src/pro_8/dbmddemo.java
@@ -0,0 +1,35 @@
+package pro_8;
+
+import java.sql.*;
+
+public class dbmddemo {
+	public static void main(String[] args) 
+	{
+		try
+		{   
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String url="jdbc:oracle:thin:@localhost:1521:XE";
+			String user="system";
+			String password="system";
+			Connection con=DriverManager.getConnection(url, user, password);
+			DatabaseMetaData dmd=con.getMetaData();
+			System.out.println(dmd.getCatalogSeparator());
+			
+			String q="select*from pstmt";
+			 ResultSet rs=con.createStatement().executeQuery(q);
+			 	System.out.println("size ="+rs.getFetchSize());
+			 	System.out.println("direction="+rs.getFetchDirection());
+			 	System.out.println("concurency="+rs.getConcurrency());
+			 	System.out.println("holdability="+rs.getHoldability());
+			 	System.out.println("rows="+rs.getRow());
+		}
+		catch(Exception e)
+		{
+			System.out.println("Exception :"+e.toString());
+	
+		}
+	
+
+	}
+
+}
